3552c0aac8a79ab34be0010c5923b13471a0316e
github username <github email address>
2019-06-25 18:34:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
-
-  get '/food_form' do
+  get "/food_form" do
     erb :food_form
   end
 
   # Add your post route and action below
-
+  post "/food" do
+    # params = {"name"=>"dstrgars", "favorite_food"=>"adgradfr"}
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
